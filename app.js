bce09a7f89f51253eae2f4e3ab58755d31e2d850
Mike Gordon <vprmatrix55@gmail.com>
2022-01-22 11:01:10 -0700

    add validation

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your name!');
+              return false;
+          }
+      }
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username",
+      message: "Enter your GitHub Username (Required)",
+      validate: githubInput => {
+          if (githubInput) {
+              return true;
+          } else {
+              console.log('Please enter GitHub Username');
+              return false;
+          }
+      }
     },
     {
       type: "input",
@@ -44,12 +60,28 @@ if (!portfolioData.projects) {
     {
       type: "input",
       name: "name",
-      message: "What is the name of your project?",
+      message: "What is the name of your project? (Required)",
+      validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Enter a project name');
+              return false;
+          }
+      }
     },
     {
       type: "input",
       name: "description",
       message: "Provide a description of the project (Required)",
+      validate: descriptionInput => {
+          if (descriptionInput) {
+              return true; 
+          } else {
+              console.log('Enter a project description');
+              return false;
+          }
+      }
     },
     {
       type: "checkbox",
@@ -69,6 +101,14 @@ if (!portfolioData.projects) {
       type: "input",
       name: "link",
       message: "Enter the GitHub link to your project. (Required)",
+      validate: linkInput => {
+          if (linkInput) {
+              return true;
+          } else {
+              console.log('Enter the GitHub link');
+              return false;
+          }
+      }
     },
     {
       type: "confirm",
